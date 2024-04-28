ed5bd4992394b9efd4c7c32400c06a648f79b224
João Guilherme <joaoguilherme.2708@gmail.com>
2024-04-28 19:31:11 -0300

    update3

--- src/Rotas.jsx
+++ src/Rotas.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import CrudCadastro from './Components/CrudCadastro/CrudCadastro';
 import Login from "./Components/CrudLogin/CrudLogin";
-import Home from "./Components/Home/Home"
+import Home from "./Components/Home/Home";
 import ListaCadastrados from "./Components/ListaCadastrados/ListaCadastrados";
 import { Routes, Route } from "react-router-dom";
 
@@ -9,8 +9,8 @@ function Rotas() {
 
     return(
         <Routes>
-            <Route path="home" element={<Home/>}/>
-            <Route path="/" element={<CrudCadastro/>}/>
+            <Route path="/" element={<Home/>}/>
+            <Route path="register" element={<CrudCadastro/>}/>
             <Route path="login" element={<Login/>}/>
             <Route path="lista" element={<ListaCadastrados/>}/>
         </Routes>
